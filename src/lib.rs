efcea9fefa897d80d7a470e8afff0caf28d55b78
Evan Almloff <evanalmloff@gmail.com>
2024-07-23 09:25:57 -0700

    parse comment nodes

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use kalosm_sample::*;
 pub enum Node {
     Element(Element),
     Text(TextNode),
+    Comment(CommentNode),
 }
 
 impl Parse for Node {
@@ -16,6 +17,7 @@ impl Parse for Node {
         Element::new_parser()
             .map_output(Self::Element)
             .or(TextNodeParser.map_output(Self::Text))
+            .or(CommentNodeParser.map_output(Self::Comment))
     }
 }
 
@@ -56,6 +58,51 @@ impl Parser for TextNodeParser {
                 _ => {
                     state.push(*c);
                 }
+            }
+        }
+
+        Ok(ParseStatus::Incomplete {
+            new_state: state,
+            required_next: std::borrow::Cow::Borrowed(""),
+        })
+    }
+}
+
+const COMMENT_START: &[u8] = b"<!--";
+const COMMENT_END: &[u8] = b"-->";
+
+#[derive(Debug, Clone)]
+pub struct CommentNode(pub String);
+
+#[derive(Debug, Clone)]
+struct CommentNodeParser;
+
+impl CreateParserState for CommentNodeParser {
+    fn create_parser_state(&self) -> Self::PartialState {
+        Vec::new()
+    }
+}
+
+impl Parser for CommentNodeParser {
+    type Output = CommentNode;
+    type PartialState = Vec<u8>;
+
+    fn parse<'a>(
+        &self,
+        state: &Self::PartialState,
+        input: &'a [u8],
+    ) -> ParseResult<ParseStatus<'a, Self::PartialState, Self::Output>> {
+        let mut state = state.clone();
+        for (i, &c) in input.iter().enumerate() {
+            if state.len() < COMMENT_START.len() && c != COMMENT_START[state.len()] {
+                bail!("comment node must start with <!--");
+            }
+            state.push(c);
+            if state.ends_with(COMMENT_END) {
+                return Ok(ParseStatus::Finished {
+                    result: CommentNode(String::from_utf8_lossy(&state).to_string()),
+                    remaining: &input[i..],
+                });
             }
         }
 
